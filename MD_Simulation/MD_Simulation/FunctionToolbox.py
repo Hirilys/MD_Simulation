81da12aac308d6a037f7e6b588c5b8bfa4502e08
Erwan <eguenier@gatech.edu>
2016-09-16 13:28:59 -0400

    Addition of class containing functions

---
+++ MD_Simulation/MD_Simulation/FunctionToolbox.py
@@ -0,0 +1,16 @@
+class FunctionToolbox(object):
+    """Class that gives a list of function to calculate neighboors"""
+import math
+
+
+Atom1 = At
+
+    def ComputeRadius():
+
+       a=1
+
+
+
+
+
+
