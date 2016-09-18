f589c66cf51fbdafc5337607dc78bd9e5d2260a3
wschuttler <wschuttler3@gatech.edu>
2016-09-18 04:00:36 -0400

    Verlet  algorithm 2.0
    Updated Verlet functions to deal with all atoms as opposed to one atom at time

--- MD_Simulation/MD_Simulation/Verlet.py
+++ MD_Simulation/MD_Simulation/Verlet.py
@@ -1,5 +1,7 @@
 #### VERLET INTEGRATOR #####
 
+### VERSION 2.0
+# Updates: All functions treat all atoms as opposed to one atom at a time
 
 import Atom as Atom
 import Potential as Potential
@@ -7,46 +9,53 @@ import math
 
 
 #################################################################################
-## Function to compute total/net forces on a single atom due to all neighboors ##
+## Function to compute total/net forces on a all atoms due to all neighboors ##
 #################################################################################
-def Calc_Forces(some_atom,Potential_LJ):
-# Input 1 : an atom
+def Calc_Forces(List_Atoms,Potential_LJ):
+# Input 1 : List of all atoms
 # Input 2: LJ potential object
 
-    # Get atom position
-    x0 = some_atom.Position[0]
-    y0 = some_atom.Position[1]
-    z0 = some_atom.Position[2]
+    #########################################################
+    # Loop over all atoms:
+    for j in range(0,len(List_Atoms)):
+        # Get atom position
+        x0 = List_Atoms[j].Position[0]
+        y0 = List_Atoms[j].Position[1]
+        z0 = List_Atoms[j].Position[2]
 
-    # Get neighbour list
-    neighbours = some_atom.neighboor
+        # Get neighbour list
+        neighbours = List_Atoms[j].neighboor
 
-    # Initialize forces
-    Fx = 0.0
-    Fy = 0.0
-    Fz = 0.0
+        # Initialize forces
+        Fx = 0.0
+        Fy = 0.0
+        Fz = 0.0
 
-    # Loop over neighborlist to sum forces
-    for i in range(0,len(neighbours)):
+        #####################################################
+        # Loop over neighborlist to sum forces
+        for i in range(0,len(neighbours)):
 
-        # Get position of i-th neighbour
-        x1 = neighbours[i].Position[0]
-        y1 = neighbours[i].Position[1]
-        z1 = neighbours[i].Position[2]
+            # Get position of i-th neighbour
+            x1 = neighbours[i].Position[0]
+            y1 = neighbours[i].Position[1]
+            z1 = neighbours[i].Position[2]
 
-        # Calculate spacing between atom and neighbor
-        del_x = x1 - x0
-        del_y = y1 - y0
-        del_z = z1 - z0
+            # Calculate spacing between atom and neighbor
+            del_x = x1 - x0
+            del_y = y1 - y0
+            del_z = z1 - z0
 
-        # Sum forces for all neighbours
-        Fx = Fx + Potential_LJ.Force_Calculation(del_x)
-        Fy = Fy + Potential_LJ.Force_Calculation(del_y)
-        Fz = Fz + Potential_LJ.Force_Calculation(del_z)
-    #End loop
+            # Sum forces for all neighbours
+            Fx = Fx + Potential_LJ.Force_Calculation(del_x)
+            Fy = Fy + Potential_LJ.Force_Calculation(del_y)
+            Fz = Fz + Potential_LJ.Force_Calculation(del_z)
+        #End neighbour loop
+        ########################################################
 
-    F_new = [Fx, Fy, Fz] # Make list of new forces on atom
-    some_atom.Update_Force(F_new) # Update atom with new forces
+        F_new = [Fx, Fy, Fz] # Make list of new forces on atom
+        List_Atoms[j].Update_Force(F_new) # Update atom with new forces
+    # End atom list loop
+    ############################################################
     return 0 # End function
 
 
@@ -54,56 +63,67 @@ def Calc_Forces(some_atom,Potential_LJ):
 #####################################################################
 ## Function to advance position and velocity using velocity Verlet ##
 #####################################################################
-def Verlet_Stepper(del_t,some_atom,mass,Potential_LJ):
+def Verlet_Stepper(del_t,List_Atoms,mass,Potential_LJ):
 # Input 1: time step
-# Input 2: an atom
+# Input 2: List containing all atoms in system
 # Input 3: atom mass
 # Input 4: LJ potential object
 
-    # Current atom position
-    x_old = some_atom.Position[0]
-    y_old = some_atom.Position[1]
-    z_old = some_atom.Position[2]
+    ###########################################################
+    # Advance position of all atoms:
+    for i in range(0,len(List_Atoms)):
+        # Current atom position
+        x_old = List_Atoms[i].Position[0]
+        y_old = List_Atoms[i].Position[1]
+        z_old = List_Atoms[i].Position[2]
 
-    # Current atome velocities (V = P/m)
-    Vx_old = some_atom.Momentum[0]/mass
-    Vy_old = some_atom.Momentum[1]/mass
-    Vz_old = some_atom.Momentum[2]/mass
+        # Current atome velocities (V = P/m)
+        Vx_old = List_Atoms[i].Momentum[0]/mass
+        Vy_old = List_Atoms[i].Momentum[1]/mass
+        Vz_old = List_Atoms[i].Momentum[2]/mass
 
-    # Current atom accelerations (a = F/m)
-    Ax_old = some_atom.Force[0]/mass
-    Ay_old = some_atom.Force[1]/mass
-    Az_old = some_atom.Force[2]/mass
+        # Current atom accelerations (a = F/m)
+        Ax_old = List_Atoms[i].Force[0]/mass
+        Ay_old = List_Atoms[i].Force[1]/mass
+        Az_old = List_Atoms[i].Force[2]/mass
 
-    # New atom position using Verlet
-    x_new = x_old + del_t*Vx_old + del_t*del_t*Ax_old/2.0
-    y_new = y_old + del_t*Vy_old + del_t*del_t*Ay_old/2.0
-    z_new = z_old + del_t*Vz_old + del_t*del_t*Az_old/2.0
+        # New atom position using Verlet
+        x_new = x_old + del_t*Vx_old + del_t*del_t*Ax_old/2.0
+        y_new = y_old + del_t*Vy_old + del_t*del_t*Ay_old/2.0
+        z_new = z_old + del_t*Vz_old + del_t*del_t*Az_old/2.0
 
-    # Update atome position
-    R_new = [x_new, y_new, z_new]
-    some_atom.Update_Position(R_new)
+        # Update atom position
+        R_new = [x_new, y_new, z_new]
+        List_Atoms[i].Update_Position(R_new)
+    # End loop over all atoms
+    ############################################################
 
-    # Recompute forces on atom
-    Calc_Forces(some_atom,Potential_LJ)
+    # Recompute forces on all atoms
+    Calc_Forces(List_Atoms,Potential_LJ)
 
-    # New acceleration computed from updated force
-    Ax_new = some_atom.Force[0]/mass
-    Ay_new = some_atom.Force[1]/mass
-    Az_new = some_atom.Force[2]/mass
+    ############################################################
+    # Advance velocities/momenta of all atoms:
+    for j in range(0,len(List_Atoms)):
+        # New acceleration computed from updated force
+        Ax_new = List_Atoms[j].Force[0]/mass
+        Ay_new = List_Atoms[j].Force[1]/mass
+        Az_new = List_Atoms[j].Force[2]/mass
 
-    # New atom velocities using Verlet
-    Vx_new = Vx_old + (Ax_old + Ax_new)*del_t/2.0
-    Vy_new = Vy_old + (Ay_old + Ay_new)*del_t/2.0
-    Vz_new = Vz_old + (Az_old + Az_new)*del_t/2.0
+        # New atom velocities using Verlet
+        Vx_new = Vx_old + (Ax_old + Ax_new)*del_t/2.0
+        Vy_new = Vy_old + (Ay_old + Ay_new)*del_t/2.0
+        Vz_new = Vz_old + (Az_old + Az_new)*del_t/2.0
 
-    # New atom momentum (P = mV)
-    Px_new = Vx_new*mass
-    Py_new = Vy_new*mass
-    Pz_new = Vz_new*mass
+        # New atom momentum (P = mV)
+        Px_new = Vx_new*mass
+        Py_new = Vy_new*mass
+        Pz_new = Vz_new*mass
 
-    P_new = [Px_new, Py_new, Pz_new] # List of new momentum
-    some_atom.Update_Momentum(P_new) # Update atom momentum
+        P_new = [Px_new, Py_new, Pz_new] # List of new momentum
+        List_Atoms[j].Update_Momentum(P_new) # Update atom momentum
+    # End loop over all atoms
+    ###############################################################
+
     return 0 # End function
 
 
--- MD_Simulation/MD_Simulation/Verlet_Test.py
+++ MD_Simulation/MD_Simulation/Verlet_Test.py
@@ -4,14 +4,22 @@ import Verlet as Verlet
 
 #### VERLET TEST CODE
 
+### Version 2.0
+# Updates: All functions treat all atoms as opposed to one atom at a time
+
+
 Potential_LJ = Potential(1.0,0.8,6.0,12.0,1.0) #Specify Potential
 Atom0 = Atom(0,[0.0,1.2,3.4]) # Create 1st atom
 Atom1 = Atom(1,[1.6,1.8,3.0]) # Create 2nd atom
 
 ## Test force function:
 Atom0.neighboor.append(Atom1) # Make 2nd atom a neighbor of 1st atom
-Verlet.Calc_Forces(Atom0,Potential_LJ) # Calculate forces on 1st atom
-print(Atom0.Force) # Output new force on 1st atom
+Atom1.neighboor.append(Atom0) # Make 1st atom a neighbor of 2nd atom
+List_Atoms = [Atom0, Atom1]
+
+Verlet.Calc_Forces(List_Atoms,Potential_LJ) # Calculate forces on both atoms
+print(List_Atoms[0].Force) # Output new force on 1st atom
+print(List_Atoms[1].Force) # Output new force on 2nd atom
 
 ## Compute forces on atom manually to validate force function:
 del_x = Atom1.Position[0] - Atom0.Position[0]
@@ -25,6 +33,9 @@ Fz = Potential_LJ.Force_Calculation(del_z)
 print([Fx,Fy,Fz]) # Output manually calculated force for comparison w/ above
 
 ## Test Verlet algorithm:
-Verlet.Verlet_Stepper(0.001,Atom0,1.0,Potential_LJ) # Advance 1st atom position & velocity
-print(Atom0.Force) # Ouput new force
-print(Atom0.Position) # Output new position
+Verlet.Verlet_Stepper(0.001,List_Atoms,1.0,Potential_LJ) # Advance position & velocity of both atoms
+print(List_Atoms[0].Force) # Ouput new force
+print(List_Atoms[0].Position) # Output new position
+
+print(List_Atoms[1].Force) # Ouput new force
+print(List_Atoms[1].Position) # Output new position
