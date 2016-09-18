de2c1f95da22a408cf58b0353a3486705e870086
Erwan <eguenier@gatech.edu>
2016-09-18 12:54:59 -0400

    Update 1.1 : Addition of neighboor calculation

--- MD_Simulation/MD_Simulation/FunctionToolbox.py
+++ MD_Simulation/MD_Simulation/FunctionToolbox.py
@@ -1,16 +1,24 @@
 class FunctionToolbox(object):
     """Class that gives a list of function to calculate neighboors"""
 import math
+from Atom import *
 
 
-Atom1 = At
+def __init__(self):
+    pass
 
-    def ComputeRadius():
+def Compute_Radius(Atom1, Atom2):
+    Radius = math.sqrt((Atom1.Position[0]-Atom2.Position[0])**2+(Atom1.Position[1]-Atom2.Position[1])**2+(Atom1.Position[1]-Atom2.Position[1])**2)
+    return Radius
 
-       a=1
+def Compute_Neighboors(List_Atoms, Rc):
+     for i in range(0,len(List_Atoms)):
+        List_Tmp = List_Atoms[:]
+        List_Tmp.remove(List_Atoms[i])
+        List_neighboors = list()
 
-
-
-
-
-
+        Atom_ref = List_Atoms[i]
+        for j in range(0,len(List_Tmp)):
+            if Compute_Radius(Atom_ref,List_Tmp[j]) <= Rc:
+                    List_neighboors.append(List_Tmp[j])
+        Atom_ref.Update_Neighboors(List_neighboors) 
--- MD_Simulation/MD_Simulation/MD_Simulation.py
+++ MD_Simulation/MD_Simulation/MD_Simulation.py
@@ -1,6 +1,11 @@
 from Atom import Atom
 from Potential import Potential
+import FunctionToolbox
 import math
+
+
+
+
 
 Potential_LJ = Potential(1.0,0.8,6,12,1)  
 u1 = Potential_LJ.PotentialCalculation(1.2)
@@ -9,4 +14,11 @@ f1 = Potential_LJ.Force_Calculation(1.2)
 List_Atoms = list()
 for x in range(0,10):
     List_Atoms.append(Atom(x,[0,0,x]))
+
+Atom1 = Atom(0,[0,0,0])
+Atom2 = Atom(1,[10,0,0])
+radius1 = FunctionToolbox.Compute_Radius(Atom1,Atom2)
+
+Liste_Atom=[Atom1,Atom2]
+FunctionToolbox.Compute_Neighboors(Liste_Atom, 2.5)
 
