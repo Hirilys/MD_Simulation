95d4e027f2d2114498b57fe255509a7bbe5af2a4
wschuttler <wschuttler3@gatech.edu>
2016-09-19 00:02:38 -0400

    Added Drew's lattice structure code

---
+++ MD_Simulation/MD_Simulation/structure.py
@@ -0,0 +1,63 @@
+from numpy import *
+import math
+
+### inputs
+# Declare supercell size/shape - [xmin, xmax, ymin, ymax, zmin, zmax]. Units are in lattice parameter value. 
+lattice = array([0, 1, 0, 1, 0, 1]) 
+# Declare structure (fcc, bcc, or sc)
+structure = "sc"
+# Declare lattice parameter 
+a = 5 # lattice constant (Angstroms)
+
+
+### Create positions
+
+# Define lattice vectors
+if structure == "fcc":
+  a1 = array([0.0, 0.5, 0.5])
+  a2 = array([0.5, 0.0, 0.5])
+  a3 = array([0.5, 0.5, 0.0])
+if structure == "bcc":
+  a1 = array([0.5, 0.5, -0.5])
+  a2 = array([0.5, 0.5, 0.5])
+  a3 = array([0.5, -0.5, 0.5])
+if structure == "sc":
+  a1 = array([1, 0, 0])
+  a2 = array([0, 1, 0])
+  a3 = array([0, 0, 1])
+a1 = a*a1;
+a2 = a*a2;
+a3 = a*a3;
+
+xstart = lattice[0]
+ystart = lattice[2]
+zstart = lattice[4]
+xlength = lattice[1]
+ylength = lattice[3]
+zlength = lattice[5]
+
+np = 8*xlength*ylength*zlength
+
+print "Number of atoms: " , np
+
+r_vec = zeros((np,3))
+#print r_vec
+count = 0
+range1 = xlength+xlength
+range2 = ylength+ylength
+range3 = zlength+zlength
+for n1 in range(0,range1):
+  for n2 in range (0,range2):
+    for n3 in range (0,range3):
+      #print n1, n2, n3
+      r = n1*a1 + n2*a2 + n3*a3
+      x = r[0]
+      y = r[1]
+      z = r[2]
+      #print r
+      r_vec[count][:] = r
+      count = count + 1
+
+r0 = r_vec
+#print r_vec
+savetxt('positions', r0, delimiter=' ')
